aac0a3c6725fea4994c7d808657a89844eb2c18a
Thomas Sablattnig <thomas.sablattnig@bitmovin.com>
2026-01-22 11:16:24 +0100

    add THEO app

---
+++ THEOplayerBasicSetup/THEOplayerBasicSetup/THEOplayerBasicSetup/THEOplayerBasicSetupApp.swift
@@ -0,0 +1,18 @@
+//
+//  THEOplayerBasicSetupApp.swift
+//  THEOplayerBasicSetup
+//
+//  Created by Thomas Sablattnig on 20.01.26.
+//
+
+import SwiftUI
+import THEOplayerCollector
+
+@main
+struct THEOplayerBasicSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
