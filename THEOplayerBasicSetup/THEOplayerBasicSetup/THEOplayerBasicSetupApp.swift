feb419d6c90479f81bbe997e3052ae0258ec30da
lukaswobak <lukas.wobak@bitmovin.com>
2026-03-03 09:30:42 +0100

    Feature/theo test app (#10)
    * add THEO app
    
    * updated contentView
    
    * basic Theo example
    
    * fixed setup
    
    * fixes
    
    * update readme
    
    * cleanup
    
    ---------
    
    Co-authored-by: Thomas Sablattnig <thomas.sablattnig@bitmovin.com>

---
+++ THEOplayerBasicSetup/THEOplayerBasicSetup/THEOplayerBasicSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  THEOplayerBasicSetupApp.swift
+//  THEOplayerBasicSetup
+//
+//  Created by Thomas Sablattnig on 20.01.26.
+//
+
+import SwiftUI
+
+@main
+struct THEOplayerBasicSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
